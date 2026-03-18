7c89d3269478dd801fe1697ed20d7b8ac84f7f20
Mayank Raj <mayank054@Loan112.com>
2026-03-18 16:21:38 +0530

    fix vite bugs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 
-  // 🔥 IMPORTANT FOR NETLIFY
-  base: "./",
+  base: "/",
 
   server: {
     port: 3000,
